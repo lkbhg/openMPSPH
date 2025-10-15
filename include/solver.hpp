01ced491d72d5dcca46dc04a72899b89f40cee10
lkbhg <74890712+lkbhg@users.noreply.github.com>
2025-10-15 19:37:45 +0800

    Update solver.hpp

--- include/solver.hpp
+++ include/solver.hpp
@@ -1,4 +1,30 @@
-﻿#ifndef __SOLVER__H
+/***************************************************************************************
+ * Project: openMPSPH
+ * File: solver.hpp
+ * Description:
+ *     This program is a Smoothed Particle Hydrodynamics (SPH) solver
+ *     developed for simulating dam break flow.
+ * Author:      Kan Liu, Master.
+ * Affiliation: School of Aerospace Engineering. Beijing Institue of Technology.
+ * Email:       lkbhg@outlook.com
+ * GitHub:      https://github.com/lkbhg
+ *
+ * Created:     October 2025
+ * Last Updated: October 2025
+ *
+ * Language:    C++17
+ * Parallelism: OpenMP
+ * Build Tool:  CMake
+ *
+ * -------------------------------------------------------------------------------------
+ * Copyright (c) 2025 Kan Liu
+ * This software is distributed under the AGPL License.
+ * If you use this software in academic work, please cite:
+ * Kan Liu, "openMPSPH: A C++ OpenMP framework for dam break", GitHub Repository, 2025.
+ * -------------------------------------------------------------------------------------
+ ***************************************************************************************/
+
+#ifndef __SOLVER__H
 #define __SOLVER__H
 
 #include <math.h>
@@ -151,5 +177,6 @@ private:
 
 	void reduction();
 };
+
 
 #endif
